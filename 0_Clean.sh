b148d263d4a93498f40453aed07a7e5328d07151
Lucas Pluta <lp.whs86@gmaii.com>
2021-10-10 20:45:23 -0400

    Add cleaning to scripts

--- 0_Clean.sh
+++ 0_Clean.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-pathToDir="Dependencies"
-
-if [ ! -d "$pathToDir/Redis" ]; then
-	mkdir -p Dependencies/_Downloads
-	mkdir -p Dependencies/Redis
-	cd Dependencies/_Downloads
-	wget https://download.redis.io/releases/redis-6.2.6.tar.gz
-	tar xzf redis-6.2.6.tar.gz --directory ../Redis --strip-components=1
-	cd ../..
-else
-	echo "Redis already exists"
+if [ -d "Dependencies" ]; then
+	rm -rf "Dependencies"
 fi
 	
-redisClient1="ServiceStack.Redis"
-if [ ! -d "$pathToDir/$redisClien12" ]; then
-	mkdir -p Dependencies/$redisClien12
-	cd Dependencies/$redisClien12
-	git clone https://github.com/ServiceStack/ServiceStack.Redis.git
-	cd ../..
-else
-	echo "$redisClient1 already exists"
-fi
-
-
-rm -rf Dependencies/_Downloads
+git clean -fxd
