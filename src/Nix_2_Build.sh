079b7789de1644927fc5a9edc64d9df1f88a86e3
Lucas Pluta <lp.whs86@gmaii.com>
2021-10-12 00:42:16 -0400

    Add protobuf build to linux.

--- src/Nix_2_Build.sh
+++ src/Nix_2_Build.sh
@@ -3,6 +3,19 @@
 if [ -d "Dependencies/Redis" ]; then
 	cd Dependencies/Redis
 	make
+	cd ../..
+else
+	echo "Redis doesn't exist!"
+fi
+
+if [ -d "Dependencies/protobuf/src" ]; then
+	cd Dependencies/protobuf/src
+	./configure
+    make
+    make check
+    sudo make install
+    sudo ldconfig # refresh shared library cache.
+	cd ../../..
 else
 	echo "Redis doesn't exist!"
 fi
