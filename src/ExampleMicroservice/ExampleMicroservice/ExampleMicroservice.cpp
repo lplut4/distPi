e5a3ee182969f1a50a530ca696a11c35aa960b66
Lucas Pluta <lp.whs86@gmaii.com>
2021-10-14 15:40:32 -0400

    Clean up.  Add return code

--- src/ExampleMicroservice/ExampleMicroservice/ExampleMicroservice.cpp
+++ src/ExampleMicroservice/ExampleMicroservice/ExampleMicroservice.cpp
@@ -1,14 +1,16 @@
+#include <algorithm>
 #include <iostream>
+#include <initializer_list>
 #include <sw/redis++/redis++.h>
 
 std::string msgTypeToString( sw::redis::Subscriber::MsgType type )
 {
-    if (type == sw::redis::Subscriber::MsgType::SUBSCRIBE)    return "SUBSCRIBE";
-    if (type == sw::redis::Subscriber::MsgType::UNSUBSCRIBE)  return "UNSUBSCRIBE";
-    if (type == sw::redis::Subscriber::MsgType::PSUBSCRIBE)   return "PSUBSCRIBE";
-    if (type == sw::redis::Subscriber::MsgType::PUNSUBSCRIBE) return "PUNSUBSCRIBE";
-    if (type == sw::redis::Subscriber::MsgType::MESSAGE)      return "MESSAGE";
-    if (type == sw::redis::Subscriber::MsgType::PMESSAGE)     return "PMESSAGE";
+    if ( type == sw::redis::Subscriber::MsgType::SUBSCRIBE    ) return "SUBSCRIBE";
+    if ( type == sw::redis::Subscriber::MsgType::UNSUBSCRIBE  ) return "UNSUBSCRIBE";
+    if ( type == sw::redis::Subscriber::MsgType::PSUBSCRIBE   ) return "PSUBSCRIBE";
+    if ( type == sw::redis::Subscriber::MsgType::PUNSUBSCRIBE ) return "PUNSUBSCRIBE";
+    if ( type == sw::redis::Subscriber::MsgType::MESSAGE      ) return "MESSAGE";
+    if ( type == sw::redis::Subscriber::MsgType::PMESSAGE     ) return "PMESSAGE";
     return "UNKNOWN";
 }
 
@@ -16,47 +18,58 @@ int main()
 {
     std::cout << "Running Example Microservice" << std::endl;
 
-    auto redisHost = "tcp://raspberrypi:6379";
+    auto redisHost = "";
+    std::vector<std::string> channelSubscriptions;
+
+    redisHost = "tcp://raspberrypi:6379";
+    channelSubscriptions.push_back( "channel-1" );
+    channelSubscriptions.push_back( "channel-2" );
+    channelSubscriptions.push_back( "channel-3" );
+
     std::cout << "Attempting to connect to " << redisHost << std::endl;
 
-    while (true) 
+    auto connectedOnce = false;
+
+    while ( true ) 
     {
         try
         {
-            auto redis = sw::redis::Redis::Redis("tcp://raspberrypi:6379");
-            auto sub = redis.subscriber();
+            auto redis = sw::redis::Redis::Redis( redisHost );
+            auto sub   = redis.subscriber();
 
-            std::cout << std::endl << "Connected!" << std::endl;
+            std::cout << std::endl << ( connectedOnce ? "Reconnected!" : "Connected!" ) << std::endl;
 
-            sub.on_message([](std::string channel, std::string msg ) 
+            sub.on_message([]( std::string channel, std::string msg ) 
                 {
                     std::cout << "Received message: " << msg << " on " << channel << std::endl;
                 });
 
-            sub.on_meta([](sw::redis::Subscriber::MsgType type, sw::redis::OptionalString channel, long long num) 
+            sub.on_meta([]( sw::redis::Subscriber::MsgType type, sw::redis::OptionalString channel, long long num ) 
                 {
-                    auto channelStr = (channel.has_value() ? channel.value() : "<NO CHANNEL>");
-                    std::cout << "Received " << msgTypeToString( type ) << " on " << channelStr << " number " << num << std::endl;
+                    std::cout << "Received " << msgTypeToString( type ) << " on " << channel.value_or( "<NO CHANNEL>" ) << " number " << num << std::endl;
                 });
 
-            sub.subscribe({ "channel-1", "channel-2", "channel-3" });
-            sub.psubscribe( "pattern1*" );
+            sub.subscribe( channelSubscriptions.begin(), channelSubscriptions.end() );
 
-            while (true) 
+            connectedOnce = true;
+
+            while ( true ) 
             {
                 try 
                 {
                     sub.consume();
                 }
-                catch (const sw::redis::Error& err) 
+                catch ( const sw::redis::Error& err ) 
                 {  
                     std::cout << "Consumer exception occurred! " << std::endl;
                 }
             }
         }
-        catch (...) 
+        catch ( ... ) 
         {
             std::cout << ".";
         }
     }
+
+    return 0;
 }
