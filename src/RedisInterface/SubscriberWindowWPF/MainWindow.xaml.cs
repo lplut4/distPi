2a1d02f14f4d4cb646e81fd6b87aa5bd9afb353c
Lucas Pluta <lp.whs86@gmaii.com>
2021-10-15 00:36:53 -0400

    Pull out redis code from main gui class.  Update .gitignore

--- src/RedisInterface/SubscriberWindowWPF/MainWindow.xaml.cs
+++ src/RedisInterface/SubscriberWindowWPF/MainWindow.xaml.cs
@@ -1,99 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using ServiceStack.Redis;
 
 namespace SubscriberWindowWPF
 {
     public partial class MainWindow : Window
     {
-
-        RedisClient m_publisher;
+        RedisClient          m_redisClient;
+        RedisDataGridAdapter m_redisGridAdapter;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            m_publisher = new RedisClient("raspberrypi");
+            m_redisClient = new RedisClient("raspberrypi");
+            m_redisGridAdapter = new RedisDataGridAdapter(this, "raspberrypi", m_dataGrid);
 
-            var args = Environment.GetCommandLineArgs();
-
-            var redisHost = "raspberrypi";
-            var channelsToSubscribe = new List<string>();
-            if ( args.Length > 2 )
-            {
-                redisHost = args[0];
-                for (int i = 1; i < args.Length; i++)
-                {
-                    channelsToSubscribe.Add(args[i]);
-                }
-            }
-            else
-            {
-                channelsToSubscribe.Add("channel-1");
-                channelsToSubscribe.Add("channel-2");
-                channelsToSubscribe.Add("channel-3");
-                channelsToSubscribe.Add("channel-4");
-            }
-
-            {
-                var column = new DataGridTextColumn();
-                column.Header = "Local Time";
-                column.Binding = new Binding("LocalTime");
-                m_dataGrid.Columns.Add(column);
-            }
-            {
-                var column = new DataGridTextColumn();
-                column.Header = "Channel";
-                column.Binding = new Binding("Channel");
-                m_dataGrid.Columns.Add(column);
-            }
-            {
-                var column = new DataGridTextColumn();
-                column.Header = "Data";
-                column.Binding = new Binding("Data");
-                m_dataGrid.Columns.Add(column);
-            }
-
-            var timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffff");
-            m_dataGrid.Items.Add(new Item() { LocalTime = timeStamp, Channel = "Connecting to ", Data = redisHost });
-
-            new Thread(() =>
-            {
-                var clientsManager = new PooledRedisClientManager( redisHost );
-                var redisPubSub = new RedisPubSubServer( clientsManager, channelsToSubscribe.ToArray() )
-                {
-                    OnMessage = (channel, msg) => recordMessage(msg, channel)
-                }
-                .Start();
-                this.Dispatcher.Invoke((Action)(() =>
-                {
-                    m_dataGrid.Items.Clear();
-                }));
-            }).Start();
-        }
-
-        private void recordMessage( string msg, string channel )
-        {
-            this.Dispatcher.Invoke((Action)(() =>
-            {
-                var timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffff");
-                m_dataGrid.Items.Add(new Item() { LocalTime = timeStamp, Channel = channel, Data = msg });
-            }));
+            m_redisGridAdapter.start();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            m_publisher.PublishMessage("channel-1", "pushed button 1");
+            m_redisClient.PublishMessage("channel-1", "pushed button 1");
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            m_publisher.PublishMessage("channel-2", "pushed button 2");
-
+            m_redisClient.PublishMessage("channel-2", "pushed button 2");
         }
 
         private void m_dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
---
+++ src/RedisInterface/SubscriberWindowWPF/RedisDataGridAdapter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceStack.Redis;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Threading;
+
+namespace SubscriberWindowWPF
+{
+    class RedisDataGridAdapter
+    { 
+        private          MainWindow m_mainWindow;
+        private readonly string     m_redisHost;
+        private          DataGrid   m_dataGrid;
+
+        public RedisDataGridAdapter( MainWindow mainWindow, string redisHost, DataGrid dataGrid )
+        {
+            m_mainWindow = mainWindow;
+            m_redisHost  = redisHost;
+            m_dataGrid   = dataGrid;
+        }
+
+        public void start()
+        {
+            var args = Environment.GetCommandLineArgs();
+
+            var redisHost = "raspberrypi";
+            var channelsToSubscribe = new List<string>();
+            if (args.Length > 2)
+            {
+                redisHost = args[0];
+                for (int i = 1; i < args.Length; i++)
+                {
+                    channelsToSubscribe.Add(args[i]);
+                }
+            }
+            else
+            {
+                channelsToSubscribe.Add("channel-1");
+                channelsToSubscribe.Add("channel-2");
+                channelsToSubscribe.Add("channel-3");
+                channelsToSubscribe.Add("channel-4");
+            }
+
+            {
+                var column = new DataGridTextColumn();
+                column.Header = "Local Time";
+                column.Binding = new Binding("LocalTime");
+                m_dataGrid.Columns.Add(column);
+            }
+            {
+                var column = new DataGridTextColumn();
+                column.Header = "Channel";
+                column.Binding = new Binding("Channel");
+                m_dataGrid.Columns.Add(column);
+            }
+            {
+                var column = new DataGridTextColumn();
+                column.Header = "Data";
+                column.Binding = new Binding("Data");
+                m_dataGrid.Columns.Add(column);
+            }
+
+            var timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffff");
+            m_dataGrid.Items.Add(new Item() { LocalTime = timeStamp, Channel = "Connecting to ", Data = m_redisHost });
+
+            new Thread(() =>
+            {
+                var clientsManager = new PooledRedisClientManager(m_redisHost);
+                var redisPubSub = new RedisPubSubServer( clientsManager, channelsToSubscribe.ToArray() )
+                {
+                    OnMessage = (channel, msg) => recordMessage(msg, channel)
+                }
+                .Start();
+                m_mainWindow.Dispatcher.Invoke((Action)(() =>
+                {
+                    m_dataGrid.Items.Clear();
+                }));
+            }).Start();
+        }
+
+        private void recordMessage(string msg, string channel)
+        {
+            m_mainWindow.Dispatcher.Invoke((Action)(() =>
+            {
+                var timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffff");
+                m_dataGrid.Items.Add(new Item() { LocalTime = timeStamp, Channel = channel, Data = msg });
+            }));
+        }
+
+    }
+}
