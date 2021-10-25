6e689c1b4932641972bea02014e17bc601d864ab
Lucas Pluta <lp.whs86@gmaii.com>
2021-10-24 22:31:52 -0400

    Organize patches by dependency

--- src/Win_1_Pull.bat
+++ src/Win_1_Pull.bat
@@ -6,11 +6,13 @@ echo #######################
 echo # RUNNING PULL SCRIPT #
 echo #######################
 
-set error=0
+set rootDir=%cd%
 
+set error=0
 if not exist "Dependencies" ( mkdir Dependencies || set error=1 )
 
 cd Dependencies
+set depDir=%cd%
 
 echo #################
 echo # Pulling Redis #
@@ -20,6 +22,7 @@ if not exist "redis" (
 ) else (
 	echo Redis already exists
 )
+cd %depDir%
 
 echo #############################
 echo # Pulling Redis for Windows #
@@ -28,10 +31,12 @@ if not exist "microsoftarchive" (
 	mkdir microsoftarchive || set error=1	
 	cd microsoftarchive
 	git clone -c advice.detachedHead=false -b win-3.0.504 https://github.com/microsoftarchive/redis.git || set error=1
-	cd ..
+	cd redis
+	git apply ..\..\..\Patches\microsoftarchive\redis\0001-Patch-to-allow-builds-in-VS2019.patch
 ) else (
 	echo Redis for Windows already exists
 )
+cd %depDir%
 
 echo ###########################
 echo # Pulling redis-plus-plus #
@@ -41,6 +46,7 @@ if not exist "redis-plus-plus" (
 ) else (
 	echo redis-plus-plus already exists
 )
+cd %depDir%
 
 echo ###############################
 echo # Pulling StackExchange.Redis #
@@ -50,27 +56,31 @@ if not exist "StackExchange.Redis" (
 ) else (
 	echo StackExchange.Redis already exists
 )
+cd %depDir%
 
 echo ####################
 echo # Pulling protobuf #
 echo ####################
-if not exist "Protobuf" (
+if not exist "protobuf" (
 	git clone -c advice.detachedHead=false -b v3.18.1 https://github.com/protocolbuffers/protobuf.git || set error=1
 	cd protobuf
 	git submodule update --init --recursive
-	cd ..
 ) else (
 	echo Protobuf already exists
 )
+cd %depDir%
 
 echo ######################
 echo # Pulling Protobuf-c #
 echo ######################
-if not exist "Dependencies\Protobuf-c" (
+if not exist "protobuf-c" (
 	git clone -c advice.detachedHead=false -b v1.4.0 https://github.com/protobuf-c/protobuf-c.git || set error=1
 ) else (
 	echo Protobuf-c already exists
 )
+cd %depDir%
+
+cd %rootDir%
 
 if "%error%" == "1" ( echo # ERRORS OCCURRED ) else ( echo # SUCCESS! )
 
--- src/Win_3_StartRedis.bat
+++ src/Win_3_StartRedis.bat
@@ -1,3 +1,3 @@
 @echo off
 
-start Dependencies\WinRedis\msvs\x64\Release\redis-server.exe Config\RedisConfig_Windows.conf || ( echo Failed to start! & pause )
+start Dependencies\microsoftarchive\redis\msvs\x64\Release\redis-server.exe Config\RedisConfig_Windows.conf || ( echo Failed to start! & pause )
