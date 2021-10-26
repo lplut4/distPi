ee9bf7f21cac589c2ed99d84d77e1c15981de849
Lucas Pluta <lp.whs86@gmaii.com>
2021-10-26 01:55:31 -0400

    shrink the windows pull script

--- src/Win_1_Pull.bat
+++ src/Win_1_Pull.bat
@@ -14,23 +14,21 @@ if not exist "Dependencies" ( mkdir Dependencies || set error=1 )
 cd Dependencies
 set depDir=%cd%
 
-echo #################
-echo # Pulling Redis #
-echo #################
+set gitConfig=-c advice.detachedHead=false
+
 if not exist "redis" (
-	git clone -c advice.detachedHead=false -b 6.2.6 https://github.com/redis/redis.git
+	echo Pulling redis...
+	git clone %gitConfig% -b 6.2.6 https://github.com/redis/redis.git
 ) else (
 	echo Redis already exists
 )
 cd %depDir%
 
-echo #############################
-echo # Pulling Redis for Windows #
-echo #############################
 if not exist "microsoftarchive" (
+	echo Pulling Redis for Windows...
 	mkdir microsoftarchive || set error=1	
 	cd microsoftarchive
-	git clone -c advice.detachedHead=false -b win-3.0.504 https://github.com/microsoftarchive/redis.git || set error=1
+	git clone %gitConfig% -b win-3.0.504 https://github.com/microsoftarchive/redis.git || set error=1
 	cd redis
 	git apply ..\..\..\Patches\microsoftarchive\redis\0001-Patch-to-allow-builds-in-VS2019.patch
 ) else (
@@ -38,45 +36,37 @@ if not exist "microsoftarchive" (
 )
 cd %depDir%
 
-echo ###########################
-echo # Pulling redis-plus-plus #
-echo ###########################
 if not exist "redis-plus-plus" (
-	git clone -c advice.detachedHead=false -b 1.3.2 https://github.com/sewenew/redis-plus-plus.git || set error=1
+	echo Pulling redis-plus-plus...
+	git clone %gitConfig% -b 1.3.2 https://github.com/sewenew/redis-plus-plus.git || set error=1
 ) else (
 	echo redis-plus-plus already exists
 )
 cd %depDir%
 
-echo ###############################
-echo # Pulling StackExchange.Redis #
-echo ###############################	
 if not exist "StackExchange.Redis" (
-	git clone -c advice.detachedHead=false -b 2.2.79 https://github.com/StackExchange/StackExchange.Redis.git || set error=1
+	echo Pulling StackExchange.Redis...
+	git clone %gitConfig% -b 2.2.79 https://github.com/StackExchange/StackExchange.Redis.git || set error=1
 ) else (
 	echo StackExchange.Redis already exists
 )
 cd %depDir%
 
-echo ####################
-echo # Pulling protobuf #
-echo ####################
 if not exist "protobuf" (
-	git clone -c advice.detachedHead=false -b v3.18.1 https://github.com/protocolbuffers/protobuf.git || set error=1
+	echo Pulling protobuf...
+	git clone %gitConfig% -b v3.18.1 https://github.com/protocolbuffers/protobuf.git || set error=1
 	cd protobuf
 	git submodule update --init --recursive
 ) else (
-	echo Protobuf already exists
+	echo protobuf already exists
 )
 cd %depDir%
 
-echo ######################
-echo # Pulling Protobuf-c #
-echo ######################
 if not exist "protobuf-c" (
-	git clone -c advice.detachedHead=false -b v1.4.0 https://github.com/protobuf-c/protobuf-c.git || set error=1
+	echo Pulling protobuf-c...
+	git clone %gitConfig% -b v1.4.0 https://github.com/protobuf-c/protobuf-c.git || set error=1
 ) else (
-	echo Protobuf-c already exists
+	echo protobuf-c already exists
 )
 cd %depDir%
 
