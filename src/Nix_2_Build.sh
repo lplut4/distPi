ff9af43cc19d5952adf2851d3d4fabcb6cc3dc56
Lucas Pluta <lp.whs86@gmail.com>
2021-10-26 02:28:41 -0400

    Update Nix Build script to build protobuf-c

--- src/Nix_2_Build.sh
+++ src/Nix_2_Build.sh
@@ -1,54 +1,60 @@
 #!/bin/bash
 
+cd Dependencies
 
-if [ -d "Dependencies/redis" ]; then
-	cd Dependencies/redis
-	echo Building Redis...
-	make
-	cd ../..
-else
-	echo "Redis doesn't exist!"
-fi
+echo Building redis...
+cd redis
+make
+cd ..
 
-if [ -d "Dependencies/redis/deps/hiredis" ]; then
-	cd Dependencies/redis/deps/hiredis
-	echo Building hiredis...
-	mkdir build
-	cd build
-	echo Running cmake...
-	cmake -Wno-dev ..
-	echo Running make...
-	make
-	cd ../../../../..
-else
-	echo "hiredis doesn't exist!"
-fi
 
-if [ -d "Dependencies/redis-plus-plus" ]; then
-	echo Building redis-plus-plus...
-	cd Dependencies/redis-plus-plus
-	mkdir build
-	cd build
-	depDir=../../Dependencies
-	hiredisDir=$depDir/Redis/deps/hiredis
-	buildDir=$hiredis/build
-	cmakeOptions="-DREDIS_PLUS_PLUS_BUILD_TEST=OFF -DREDIS_PLUS_PLUS_BUILD_SHARED=OFF -DINCLUDEDIR=$depDir/Redis/deps/ -DHIREDIS_HEADER=$depDir/Redis/deps -DHIREDIS_LIB=$buildDir/libhiredis_static.a -DTEST_HIREDIS_LIB=$buildDir/hiredis-test -DCMAKE_PREFIX_PATH=$depDir/Redis/deps/ -DREDIS_PLUS_PLUS_CXX_STANDARD=11"
-	echo Running cmake...
-	cmake $cmakeOptions ..
-	echo Running make...
-	make
-	cd ../../..
-else
-	echo "redis-plus-plus doesn't exist!"
-fi
+echo Building hiredis...
+cd redis/deps/hiredis
+mkdir build
+cd build
+echo Running cmake...
+cmake -Wno-dev ..
+echo Running make...
+make
+cd ../../../..
 
-if [ -d "Dependencies/protobuf/src" ]; then
-	cd Dependencies/protobuf
-	echo Building Protobuf..
-	./configure
-    make
-    make check
-	cd ../..
-else
-	echo "protobuf doesn't exist!"
+
+echo Building redis-plus-plus...
+cd redis-plus-plus
+mkdir build
+cd build
+depDir=../..
+hiredisDir=$depDir/redis/deps/hiredis
+buildDir=$hiredis/build
+cmakeOptions="-DREDIS_PLUS_PLUS_BUILD_TEST=OFF -DREDIS_PLUS_PLUS_BUILD_SHARED=OFF -DHIREDIS_HEADER=$depDir/redis/deps -DHIREDIS_LIB=$buildDir/libhiredis_static.a -DTEST_HIREDIS_LIB=$buildDir/hiredis-test -DCMAKE_PREFIX_PATH=$depDir/redis/deps/ -DREDIS_PLUS_PLUS_CXX_STANDARD=11"
+cmake $cmakeOptions ..
+make
+cd ../..
+
+
+echo Building Protobuf...
+cd protobuf
+./configure
+make
+# make check
+cd ..
+
+
+echo Building Protobuf-c...
+cd protobuf-c
+if [ ! -d "google/protobuf" ]; then
+	mkdir google
+	cd google
+	mkdir protobuf
+	cd ..
 fi
+cp ../protobuf/src/google/protobuf/descriptor.proto google/protobuf/
+cd build-cmake/
+mkdir build
+cd build
+set cmakeArgs=-DPROTOBUF_PROTOC_EXECUTABLE=../../../protobuf/src/protoc -DCMAKE_BINARY_DIR=../../../protobuf/src/ -DProtobuf_LIBRARIES=../../../protobuf/src/ -DProtobuf_INCLUDE_DIR=../../../protobuf/src/ -DPROTOBUF_LIBRARY=../../../protobuf/src/libprotobuf.la -DPROTOBUF_PROTOC_LIBRARY=../../../protobuf/src/libprotoc.la
+cmake $cmakeArgs ..
+make
+cd ../../..
+
+cd ..
