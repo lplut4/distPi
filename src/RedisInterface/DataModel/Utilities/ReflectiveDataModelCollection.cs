f6b01cbadbc780ac636a65bbb37bc6b7c9225bbe
Lucas Pluta <lp.whs86@gmaii.com>
2021-11-02 22:26:05 -0400

    Small refactor of ReflectiveDataModelCollection

--- src/RedisInterface/DataModel/Utilities/ReflectiveDataModelCollection.cs
+++ src/RedisInterface/DataModel/Utilities/ReflectiveDataModelCollection.cs
@@ -7,50 +7,39 @@ namespace DataModel
 {
     public class ReflectiveDataModelCollection
     {
-        private static List<Type> serializableTypes = Assembly.GetExecutingAssembly().GetTypes()
-                                                              .Where(t => t.Namespace.StartsWith("DataModel"))
-                                                              .Where(t => !t.Name.EndsWith("Reflection"))
-                                                              .Where(t => !t.Name.Any(ch => !Char.IsLetterOrDigit(ch)))
-                                                              .Where(t => !t.FullName.Any(ch => ch == '+'))
-                                                              .Where(t => !t.Name.Equals("Types"))
-                                                              .Where(t => !t.Name.Equals("ReflectiveDataModelCollection"))
-                                                              .ToList();
+        private static readonly List<Type>               SERIALIZABLE_TYPES;
+        private static readonly Dictionary<string, Type> SERIALIZABLE_TYPES_BY_NAME;
 
-        private static bool dictInit = false;
+        static ReflectiveDataModelCollection()
+        {
+            SERIALIZABLE_TYPES = Assembly.GetExecutingAssembly().GetTypes()
+                                                                .Where(t => t.Namespace.StartsWith("DataModel"))
+                                                                .Where(t => !t.Name.EndsWith("Reflection"))
+                                                                .Where(t => !t.Name.Any(ch => !Char.IsLetterOrDigit(ch)))
+                                                                .Where(t => !t.FullName.Any(ch => ch == '+'))
+                                                                .Where(t => !t.Name.Equals("Types"))
+                                                                .Where(t => !t.Name.Equals("ReflectiveDataModelCollection"))
+                                                                .ToList();
 
-        private static Dictionary<string, Type> serializableTypesByFullName = new Dictionary<string, Type>();
+            SERIALIZABLE_TYPES_BY_NAME = new Dictionary<string, Type>();
+            foreach (var dataType in SERIALIZABLE_TYPES)
+            {
+                SERIALIZABLE_TYPES_BY_NAME.Add(dataType.FullName, dataType);
+            }
+        }
 
         public static List<Type> GetSerializableTypes()
         {
-            if (!dictInit)
-            {
-                foreach (var dataType in serializableTypes)
-                {
-                    serializableTypesByFullName.Add(dataType.FullName, dataType);
-                }
-                dictInit = true;
-            }
-
-            return serializableTypes;
+            return SERIALIZABLE_TYPES;
         }
 
         public static Type GetSerializableType( string fullName )
         {
-            if (!dictInit)
-            {
-                foreach ( var dataType in serializableTypes )
-                {
-                    serializableTypesByFullName.Add(dataType.FullName, dataType);
-                }
-                dictInit = true;
-            }
-
-            if ( !serializableTypesByFullName.ContainsKey(fullName) )
+            if ( !SERIALIZABLE_TYPES_BY_NAME.ContainsKey(fullName) )
             {
                 return null;
             }
-
-            return serializableTypesByFullName[fullName];
+            return SERIALIZABLE_TYPES_BY_NAME[fullName];
         }
 
         public static byte[] Serialize(Google.Protobuf.IMessage message)
