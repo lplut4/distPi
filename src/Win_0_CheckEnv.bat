4b88c6a6e032e24f3f760716f8028d550b0e07a5
Lucas Pluta <lp.whs86@gmaii.com>
2021-11-05 01:01:47 -0400

    Minimize environmental script

--- src/Win_0_CheckEnv.bat
+++ src/Win_0_CheckEnv.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 title 0_SetEnv.bat
 
@@ -8,46 +8,23 @@ if "%INIT%"=="" (
 	set PRINT_INFO=true
 )
 
-set MS_BUILD=""
-set MS_TEST=""
-set VS_SETUP_DEV=""
-
 set errorSetEnv=0
 
-if not exist %MS_BUILD% (
-	set MS_BUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
-	set MS_TEST="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-	set VS_SETUP_DEV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-)
-
-if not exist %MS_BUILD% (
-	set MS_BUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
-	set MS_TEST="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
-	set VS_SETUP_DEV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
-)
-
-if not exist %MS_BUILD% (
-	set MS_BUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
-	set MS_TEST="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
-	set VS_SETUP_DEV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
-)
-
-if not exist %MS_BUILD% (
-	set MS_BUILD="D:\Program Files (x86)\Microsoft Visual Studio\VS2019\MSBuild\Current\Bin\MSBuild.exe"
-	set MS_TEST="D:\Program Files (x86)\Microsoft Visual Studio\VS2019\Common7\Tools\VsDevCmd.bat"
-	set VS_SETUP_DEV="D:\Program Files (x86)\Microsoft Visual Studio\VS2019\Common7\Tools\VsDevCmd.bat"
-)
-
+set MS_BUILD=""
+if not exist %MS_BUILD% ( set MS_BUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" )
+if not exist %MS_BUILD% ( set MS_BUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" )
+if not exist %MS_BUILD% ( set MS_BUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" )
+if not exist %MS_BUILD% ( set MS_BUILD="D:\Program Files (x86)\Microsoft Visual Studio\VS2019\MSBuild\Current\Bin\MSBuild.exe" )
 if not exist %MS_BUILD% ( 
-	echo Failed to find MSBuild
+	echo Failed to find MSBuild 
 	set errorSetEnv=1
 )
 
-if not exist %MS_TEST% ( 
-	echo Failed to find MSTest
-	set errorSetEnv=1
-)
-
+set VS_SETUP_DEV=""
+if not exist %VS_SETUP_DEV% ( set VS_SETUP_DEV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" )
+if not exist %VS_SETUP_DEV% ( set VS_SETUP_DEV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat" )
+if not exist %VS_SETUP_DEV% ( set VS_SETUP_DEV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" )
+if not exist %VS_SETUP_DEV% ( set VS_SETUP_DEV="D:\Program Files (x86)\Microsoft Visual Studio\VS2019\Common7\Tools\VsDevCmd.bat" )
 if not exist %VS_SETUP_DEV% ( 
 	echo Failed to find VsDevCmd.bat 
 	set errorSetEnv=1
