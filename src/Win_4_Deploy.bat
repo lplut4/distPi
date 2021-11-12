c391513fe0963006ea2432b66064b000d0f3785d
Lucas Pluta <lp.whs86@gmaii.com>
2021-11-12 16:00:51 -0500

    Add initial deploy script

---
+++ src/Win_4_Deploy.bat
@@ -0,0 +1,34 @@
+@echo off
+
+setlocal enableextensions
+
+set rootDir=%cd%
+
+cd ..
+mkdir build
+cd build
+
+mkdir Config
+mkdir Dependencies\microsoftarchive\redis\msvs\x64\Release\
+mkdir ExampleMicroservice\build\Release\
+mkdir MonitoringTools\ChronologicalMessageWindow\bin\Release\
+mkdir MonitoringTools\CommandLineLogViewer\bin\Release\
+
+copy %rootDir%\Win_3_*.bat .
+
+copy %rootDir%\Config\RedisConfig_Windows.conf Config
+
+copy %rootDir%\Dependencies\microsoftarchive\redis\msvs\x64\Release\*.exe Dependencies\microsoftarchive\redis\msvs\x64\Release
+
+copy %rootDir%\MonitoringTools\CommandLineLogViewer\bin\Release\*.dll MonitoringTools\CommandLineLogViewer\bin\Release
+copy %rootDir%\MonitoringTools\CommandLineLogViewer\bin\Release\*.exe MonitoringTools\CommandLineLogViewer\bin\Release
+
+copy %rootDir%\MonitoringTools\ChronologicalMessageWindow\bin\Release\*.dll MonitoringTools\ChronologicalMessageWindow\bin\Release
+copy %rootDir%\MonitoringTools\ChronologicalMessageWindow\bin\Release\*.exe MonitoringTools\ChronologicalMessageWindow\bin\Release
+
+copy %rootDir%\ExampleMicroservice\build\Release\*.exe ExampleMicroservice\build\Release
+copy %rootDir%\ExampleMicroservice\build\Release\*.dll ExampleMicroservice\build\Release
+
+endlocal
+
+pause
