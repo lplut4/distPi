d8271b12f8213eb0d02a86734e86972ab1439916
Lucas Pluta <lp.whs86@gmaii.com>
2021-11-12 18:51:58 -0500

    Script cleanup

--- src/Nix_1_Pull.sh
+++ src/Nix_1_Pull.sh
@@ -18,31 +18,12 @@ if [ ! -d "redis" ]; then
 else
     echo "Redis already exists"
 fi
-    
-if [ ! -d "microsoftarchive" ]; then
-    echo "Pulling Redis for Windows..."
-    mkdir microsoftarchive
-    cd microsoftarchive
-    git clone $gitConfig -b win-3.0.504 https://github.com/microsoftarchive/redis.git
-    cd redis
-    git apply ../../../Patches/microsoftarchive/redis/0001-Patch-to-allow-builds-in-VS2019.patch
-    cd ../..
-else
-    echo "Redis for Windows already exists"
-fi
-    
+        
 if [ ! -d "redis-plus-plus" ]; then
     echo "Pulling redis-plus-plus..."
     git clone $gitConfig -b 1.3.2 https://github.com/sewenew/redis-plus-plus.git
 else
     echo "redis-plus-plus already exists"
-fi
-
-if [ ! -d "StackExchange.Redis" ]; then
-    echo "Pulling StackExchange.Redis..."
-    git clone $gitConfig -b 2.2.79 https://github.com/StackExchange/StackExchange.Redis.git
-else
-    echo "StackExchange.Redis already exists"
 fi
 
 if [ ! -d "protobuf" ]; then
--- src/Win_4_Deploy.bat
+++ src/Win_4_Deploy.bat
@@ -15,17 +15,12 @@ mkdir MonitoringTools\ChronologicalMessageWindow\bin\Release\
 mkdir MonitoringTools\CommandLineLogViewer\bin\Release\
 
 copy %rootDir%\Win_3_*.bat .
-
 copy %rootDir%\Config\RedisConfig_Windows.conf Config
-
 copy %rootDir%\Dependencies\microsoftarchive\redis\msvs\x64\Release\*.exe Dependencies\microsoftarchive\redis\msvs\x64\Release
-
 copy %rootDir%\MonitoringTools\CommandLineLogViewer\bin\Release\*.dll MonitoringTools\CommandLineLogViewer\bin\Release
 copy %rootDir%\MonitoringTools\CommandLineLogViewer\bin\Release\*.exe MonitoringTools\CommandLineLogViewer\bin\Release
-
 copy %rootDir%\MonitoringTools\ChronologicalMessageWindow\bin\Release\*.dll MonitoringTools\ChronologicalMessageWindow\bin\Release
 copy %rootDir%\MonitoringTools\ChronologicalMessageWindow\bin\Release\*.exe MonitoringTools\ChronologicalMessageWindow\bin\Release
-
 copy %rootDir%\ExampleMicroservice\build\Release\*.exe ExampleMicroservice\build\Release
 copy %rootDir%\ExampleMicroservice\build\Release\*.dll ExampleMicroservice\build\Release
 
