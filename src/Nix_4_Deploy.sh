2da1ea8a0a2baab75c5f1ba41a8ebc39e18339cc
Lucas Pluta <lp.whs86@gmail.com>
2021-11-12 19:09:12 -0500

    Fix up mistakes in Linux deploy script:

--- src/Nix_4_Deploy.sh
+++ src/Nix_4_Deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set rootDir=$(pwd)
+rootDir=$(pwd)
+
+echo $rootDir
 
 cd ..
 mkdir build
@@ -10,7 +12,7 @@ mkdir -p Config
 mkdir -p Dependencies/redis/src
 mkdir -p ExampleMicroservice/build 
 
-copy $rootDir/Nix_3_*.sh .
-copy $rootDir/Config/RedisConfig_Linux.conf Config
-copy $rootDir/Dependencies/redis/src/redis-* Dependencies/redis/src
-copy $rootDir/ExampleMicroservice/build/ExampleMicroservice ExampleMicroservice/build
+cp $rootDir/Nix_3_*.sh .
+cp $rootDir/Config/RedisConfig_Linux.conf Config
+cp $rootDir/Dependencies/redis/src/redis-* Dependencies/redis/src
+cp $rootDir/ExampleMicroservice/build/ExampleMicroservice ExampleMicroservice/build
