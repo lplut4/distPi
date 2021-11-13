d0e77babed53fb7725ee36bf70f8a00f7e5d3312
Lucas Pluta <lp.whs86@gmail.com>
2021-11-12 19:11:50 -0500

    Be more specific with the deploy, to avoid pulling in source files and object files

--- src/Nix_4_Deploy.sh
+++ src/Nix_4_Deploy.sh
@@ -14,5 +14,12 @@ mkdir -p ExampleMicroservice/build
 
 cp $rootDir/Nix_3_*.sh .
 cp $rootDir/Config/RedisConfig_Linux.conf Config
-cp $rootDir/Dependencies/redis/src/redis-* Dependencies/redis/src
+cp $rootDir/Dependencies/redis/src/redis-benchmark Dependencies/redis/src
+cp $rootDir/Dependencies/redis/src/redis-check-aof Dependencies/redis/src
+cp $rootDir/Dependencies/redis/src/redis-check-rdb Dependencies/redis/src
+cp $rootDir/Dependencies/redis/src/redis-cli Dependencies/redis/src
+cp $rootDir/Dependencies/redis/src/redis-sentinel Dependencies/redis/src
+cp $rootDir/Dependencies/redis/src/redis-server Dependencies/redis/src
 cp $rootDir/ExampleMicroservice/build/ExampleMicroservice ExampleMicroservice/build
+
+
